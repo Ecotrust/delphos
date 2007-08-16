8b06dedf649725420e7c9b3c86111a5f96a54c95
Tim Welch <tim.j.welch@gmail.com>
2007-08-16 17:20:30 +0000

    'updated

--- src/winsetup.py
+++ src/winsetup.py
@@ -1,13 +1,19 @@
 from py2exe.build_exe import py2exe
 from distutils.core import setup
- 
+
+import matplotlib
+
 opts = {
     "py2exe": {
         "includes": [],
-        "packages": ["sqlalchemy", "pyExcelerator"],
+	"excludes": ['backend_gtkagg', 'backend_wxagg'],
+	"dll_excludes": ['libgdk_pixbuf-2.0-0.dll', 'libgobject-2.0-0.dll', 'libgdk-win32-2.0-0.dll'],
+        "packages": ["sqlalchemy", "pyExcelerator", "matplotlib", "pytz"],
         "dist_dir": "bin",
     }
 }
  
 setup(options = opts,
-      console=[{"script": "start_delphos.py"}] )
+      console=[{"script": "start_gui.py"}],
+      data_files=[matplotlib.get_py2exe_datafiles()]
+)
